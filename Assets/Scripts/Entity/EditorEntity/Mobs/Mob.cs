79a97d5b365b6223680361e4067742f2507bec11
slidrex <108924768+slidrex@users.noreply.github.com>
2023-07-08 16:05:29 +0500

    Level Entities

--- Assets/Scripts/Entity/EditorEntity/Mobs/Mob.cs
+++ Assets/Scripts/Entity/EditorEntity/Mobs/Mob.cs
@@ -2,7 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Mob : Entity
+public abstract class Mob : Entity, IStatProvider
 {
-    public IStatProvider Stats;
+    public abstract ushort Health { get; }
+
+    public abstract float AttackDistance { get; }
+
+    public abstract float AttackSpeed { get; }
+
+    public abstract float MovementSpeed { get; }
+
+    private void Update()
+    {
+        Vector2 playerPos = Player.Instance.GetPosition();
+        float distSqr = Vector2.SqrMagnitude((Vector2)transform.position - playerPos);
+        if(distSqr > AttackDistance * AttackDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerPos, MovementSpeed * Time.deltaTime);
+        }
+    }
 }
--- Assets/Scripts/Entity/EditorEntity/Mobs/Mobs/Dummy.cs
+++ Assets/Scripts/Entity/EditorEntity/Mobs/Mobs/Dummy.cs
@@ -5,5 +5,12 @@ using UnityEngine;
 public class Dummy : Mob
 {
     public override byte SpaceRequired => 2;
-    
+
+    public override ushort Health => 100;
+
+    public override float AttackDistance => 1;
+
+    public override float AttackSpeed => 1;
+
+    public override float MovementSpeed => 1;
 }
